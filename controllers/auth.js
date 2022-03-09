bb56e53f92692835d06145ecb86940f011adc207
Sinhue González <sinhue.glez@gmail.com>
2022-03-09 13:47:37 -0600

    Google Sing In Agregado

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const bcryptjs = require("bcryptjs");
 const { response } = require("express");
+const { json } = require("express/lib/response");
 const { generarJWT } = require("../helpers/generarJWT");
+const { googleVerify } = require("../helpers/google-verify");
 const Usuario = require('../models/usuario');
 
 const login = async (req, res = response) => {
@@ -43,6 +45,51 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async (req, res = response) => {
+    const {id_token} = req.body;
+    try {
+        const { nombre, img, correo } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+        
+        if(!usuario){
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                rol: 'USER_ROLE',
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        // Si el usuario en BD tiene estado en false negar autenticación
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador - Usuario bloqueado '
+            });
+        }
+        // generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+        
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token de Google no se pudo verificar',
+            error
+        });
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
